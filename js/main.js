7e8be7f57fc741334ea2f3008b8a7f0713a24a57
Timothy Radder <timo0060@gmail.com>
2016-04-16 12:06:03 -0400

    Set up base project. Spaceship moves

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, '', {preload:preload, create:create, update:update});
+var player;
+
+var background_one;
+var background_two;
+
+var player_x_mod = 0;
+var player_y_mod = 0;
+
+function preload(){
+    game.load.image('spaceShip', 'assets/images/spaceShip.png');
+    game.load.image('space', 'assets/images/space.png');
+}
+
+function create(){
+    background_one = game.add.sprite(0,0, 'space');
+    background_two = game.add.sprite(1000,0, 'space');
+    player = game.add.sprite(20,120, 'spaceShip');
+    
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    game.physics.arcade.enable(player);
+    game.physics.arcade.enable(background_one);
+    game.physics.arcade.enable(background_two);
+    
+    
+}
+
+function update(){
+    
+    
+    var cursors = game.input.keyboard.createCursorKeys();
+    
+    player.body.velocity.x = player_x_mod;
+    player.body.velocity.y = player_y_mod;
+    
+    if(cursors.left.isDown){
+       player_x_mod = -100;
+    }else if(cursors.right.isDown){
+        player_x_mod = 250
+    }else{
+        if(player.body.velocity.x > 0){
+            player_x_mod -=10;            
+        }else if(player.body.velocity.x < 0){
+            player_x_mod +=5; 
+        }
+    }
+    
+    if(cursors.up.isDown){
+        player_y_mod = -150;
+    }else if(cursors.down.isDown){
+        player_y_mod = 150;
+    }else{
+        if(player.body.velocity.y > 0){
+            player_y_mod -=10;            
+        }else if(player.body.velocity.y < 0){
+            player_y_mod +=10; 
+        }
+    }
+}
